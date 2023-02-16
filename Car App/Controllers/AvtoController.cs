0339590bab2b4b5803cd0e99b68d31e707e3891a
Gal Verlič <galverlic3@gmail.com>
2023-02-16 11:09:46 +0100

    Add project files.

---
+++ Car App/Controllers/AvtoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AvtoController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetAvto()
+        {
+            string[] avto = { "Volkswagen, Golf, 2015", "BMW, 520i, 2011", "Renault, Clio, 2013" };
+            return avto;
+        }
+    }
+}
---
+++ Car App/Models/Avto.cs
@@ -0,0 +1,17 @@
+﻿namespace Car_App.Models
+{
+    public class Avto
+    {
+        public long Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int FirstRegistration { get; set; }
+        public int ProductionYear { get; set; }
+        public int Mileage { get; set; }
+        public string FuelType { get; set; }
+        public int Power { get; set; }
+        
+
+
+    }
+}
