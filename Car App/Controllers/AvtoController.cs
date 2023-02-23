d318cf9df5f7bf56e84d019374859dc20f68c6e1
Gal Verlič <galverlic3@gmail.com>
2023-02-23 09:16:12 +0100

    commit

--- Car App/Controllers/AvtoController.cs
+++ Car App/Controllers/AvtoController.cs
@@ -1,6 +1,8 @@
-﻿using Car_App.Models;
+﻿using Car_App.Context;
+using Car_App.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
 
 namespace Car_App.Controllers
 {
@@ -8,14 +10,51 @@ namespace Car_App.Controllers
     [ApiController]
     public class AvtoController : ControllerBase
     {
+
+        private readonly DatabaseContext _dbContext;
+
+        public AvtoController(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/Cars
+
+        // getter search  po vseh avtih
         [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<Avto>>> GetCars()
+            {
+            if (_dbContext.Cars == null)
+
+            {
+                return NotFound();
+            }
+            return await _dbContext.Cars.ToListAsync();
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Avto>> GetCar(int id)
+        {
+            var car = await _dbContext.Cars.FindAsync(id);
+
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return car;
+        }
+
+
         public ActionResult GetAvto([FromQuery]int count)
         {
             Avto[] avto =
             {
                 new() { Title = "Citroen C-Elysee Seduction HDi 92 BVM"},
                 new() { Title = "Renault Twingo 1.2 16V .TEMPOMAT.."},
-                new() { Title = "Audi Q3 35 TFSI S-Tronic Advanced 150KM COCKPIT Full LED" }
+                new() { Title = "Audi Q3 35 TFSI S-Tronic Advanced 150KM COCKPIT Full LED"}
 
             };
             
--- Car App/Program.cs
+++ Car App/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DatabaseContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseContext")));
 
 var app = builder.Build();
 
