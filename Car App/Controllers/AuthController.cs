220831fd0ff2ade9fa4880cd07ee946270399b40
Gal Verlič <galverlic3@gmail.com>
2023-04-07 14:28:30 +0200

    authControlelr

---
+++ Car App/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using Car_App.Data.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    private readonly DatabaseContext _dbContext;
+
+    public AuthController(IConfiguration configuration, DatabaseContext dbContext)
+    {
+        _configuration = configuration;
+        _dbContext = dbContext;
+    }
+
+    [HttpPost("token")]
+    public IActionResult Token([FromBody] LoginModel model)
+    {
+        // Validate the user's credentials against the database
+        var user = _dbContext.Users.FirstOrDefault(u => u.Username == model.Username);
+
+        if (user == null || !PasswordHasher.VerifyPassword(model.Password, user.PasswordHash))
+        {
+            return Unauthorized("Invalid credentials");
+        }
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var secretKey = _configuration["JwtSettings:SecretKey"];
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+        var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["JwtSettings:Issuer"],
+            audience: _configuration["JwtSettings:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["JwtSettings:ExpirationMinutes"])),
+            signingCredentials: signingCredentials
+        );
+
+        return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+    }
+}
