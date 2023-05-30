8cb23eddd1d101299a852670129ccae77caa975d
Gal Verlič <galverlic3@gmail.com>
2023-05-30 09:52:34 +0200

    unit tests complete

--- TestProject1/Service/OwnerServiceTests.cs
+++ TestProject1/Service/OwnerServiceTests.cs
@@ -1,1 +1,65 @@
-﻿
+﻿using Car_App.Data.Context;
+using Car_App.Data.Models;
+using Car_App.Helpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Moq;
+using Moq.EntityFrameworkCore;
+
+public class OwnerServiceTests
+{
+    private readonly Mock<DbSet<Car>> _mockSetCar;
+    private readonly Mock<DbSet<Owner>> _mockSetOwner;
+    private readonly Mock<DatabaseContext> _mockContext;
+    private readonly IOptions<JwtSettings> _jwtSettings;
+    private readonly Guid _ownerId;
+    private readonly Owner _owner;
+    private readonly List<Car> _cars;
+
+    public OwnerServiceTests()
+    {
+        _mockSetOwner = new Mock<DbSet<Owner>>();
+
+        _mockSetCar = new Mock<DbSet<Car>>();
+        _mockContext = new Mock<DatabaseContext>();
+        _jwtSettings = Options.Create(new JwtSettings()); // Assuming JwtSettings is a simple POCO
+        _ownerId = Guid.NewGuid();
+        _owner = new Owner { Id = _ownerId };
+        _cars = new List<Car> // Instantiate _cars list here
+    {
+        new Car { OwnerId = _ownerId },
+        new Car { OwnerId = _ownerId }
+    };
+    }
+
+
+    [Fact]
+    public async Task GetOwnerByIdAsyncTest()
+    {
+        // Arrange
+        _mockContext.Setup(c => c.Owners.FindAsync(_ownerId)).ReturnsAsync(_owner);
+        var service = new OwnerService(_mockContext.Object, _jwtSettings);
+
+        // Act
+        var result = await service.GetOwnerByIdAsync(_ownerId);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(_ownerId, result.Id);
+    }
+
+    [Fact]
+    public async Task GetCarsByOwnerIdAsyncTest()
+    {
+        // Arrange
+        _mockContext.Setup(x => x.Cars).ReturnsDbSet(_cars);
+        var service = new OwnerService(_mockContext.Object, _jwtSettings);
+
+        // Act
+        var result = await service.GetCarsByOwnerIdAsync(_ownerId);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Count()); // Assuming there are 2 cars with the same ownerId
+    }
+}
